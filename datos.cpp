c8836a374be664045ff69d883bf2b8a00c8b46da
Cesar de Jesus Becerra Vera <cesar.becerra8711@alumnos.udg.mx>
2025-02-03 16:10:16 -0600

    Mi Primer Commit

---
+++ datos.cpp
@@ -0,0 +1,22 @@
+/**************************************** PRESENTACIÓN **************************************** 
+NOMBRE: Cesar de Jesus Becerra Vera 
+FECHA: 30 de Enero de 2025
+PROGRAMA: datos.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+PROFESOR: Sergio Franco Casillas
+DESCRIPCION: Imprimir una presentacion con mis datos en la consola 
+***********************************************************************************************/
+
+#include <stdio.h> // Incluimos la libreria de entrada y salida
+#include <conio.h> // Incluimos la libreria de entrada y salida de consola
+
+main() { // Funcion principal del programa
+    printf("Nombre: Cesar de Jesus Becerra Vera\n");
+    printf("Edad: 19 Años\n");
+    printf("Lugar de Residencia: Privada Maple #1976, Fraccionamiento Los Sauses, Tepatitlan de Morelos, Jalisco\n");
+    printf("Fecha: 30 de Enero de 2025\n"); 
+    printf("Carrera: Ingenieria en Computacion\n");
+    printf("Semestre: Segundo\n");
+    getch(); // Esperamos a que el usuario presione una tecla para cerrar la consola
+}
---
+++ hola_mundo.cpp
@@ -0,0 +1,17 @@
+/**************************************** PRESENTACIÓN **************************************** 
+NOMBRE: Cesar de Jesus Becerra Vera 
+FECHA: 3 de Febrero de 2025
+PROGRAMA: hola_mundo.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+INGENIERIA EN COMPUTACION / 2DO SEMESTRE
+PROFESOR: Sergio Franco Casillas
+DESCRIPCION: Imprimir un "hola mundo" en la consola 
+***********************************************************************************************/
+
+#include <stdio.h> // Incluimos la libreria de entrada y salida
+#include <conio.h> // Incluimos la libreria de entrada y salida de consola
+
+main() { // Funcion principal del programa
+    printf("\nHola Mundo\n"); // Imprimimos un "Hola Mundo" en la consola
+    getch(); // Esperamos a que el usuario presione una tecla para cerrar la consola
+}
